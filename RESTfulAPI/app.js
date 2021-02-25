a1c76df581a84e063464d760d717f201eb54be5f
Sunil kumar Sharma <32214489+geekyskr@users.noreply.github.com>
2021-02-25 12:15:55 +0530

    Update app.js

--- RESTfulAPI/app.js
+++ RESTfulAPI/app.js
@@ -9,12 +9,11 @@ const orderRoutes = require("./api/routes/orders");
 const userRoutes = require('./api/routes/user');
 
 mongoose.connect(
-  "mongodb://node-shop:" +
-    process.env.MONGO_ATLAS_PW +
-    "@node-rest-shop-shard-00-00-wovcj.mongodb.net:27017,node-rest-shop-shard-00-01-wovcj.mongodb.net:27017,node-rest-shop-shard-00-02-wovcj.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin",
+  'mongodb+srv://geekyskr:'+'MongodB3007'+'@node-rest-shop.2y2vu.mongodb.net/<dbname>?retryWrites=true&w=majority',
   {
-    useMongoClient: true
-  }
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}
 );
 mongoose.Promise = global.Promise;
 
